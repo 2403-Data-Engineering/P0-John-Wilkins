b8e5fd7b0d367f0add1359736826ff275e4f3105
John Wilkins <wilkinsjohnstanley@gmail.com>
2026-04-01 14:07:55 -0500

    starting over

--- main.py
+++
@@ -1,8 +0,0 @@
-
-def render():
-    print("Enter a first name: ")
-    first_name = input()
-    print("Enter a last name: ")
-    last_name = input()
-    new_student = Student(first_name, last_name)
-    navigate(New_)
