a77854c5d406c9c79f3be8182b962747546f0248
John Wilkins <wilkinsjohnstanley@gmail.com>
2026-04-01 09:33:47 -0500

    Sample 3-layer architecture program

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
-def render():
-    print("Enter a first name: ")
-    first_name = input()
-    print("Enter a last name: ")
-    last_name = input()
-    new_student = Student(first_name, last_name)
-    navigate(New_)
+from data import automobile_repo
+from data.models import Car, Truck, SUV
+from presentation import menu
+
+automobile_repo.seed_inventory([
+    Car("BMW", 2020, 20000, 4),
+    Car("Honda", 2005, 2000, 4),
+    Car("Ford", 2012, 5000, 2),
+    Truck("Toyota", 2021, 15000, "4WD"),
+    Truck("Ford", 2010, 6000, "FWD"),
+    Truck("Chevrolet", 2021, 15000, "RWD"),
+    SUV("Tesla", 2024, 30000, 5),
+    SUV("Nissan", 1995, 1500, 5),
+    SUV("Hyundai", 2009, 6500, 5),
+])
+
+menu.run()
